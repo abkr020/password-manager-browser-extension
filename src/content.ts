26e57a3d362e07033ae8de00999d9f6898ef6251
abkr020 <abkr020@gmail.com>
2026-08-28 10:44:18 +0530

    https://mail.slvai.tech/login tested on this

--- src/content.ts
+++ src/content.ts
@@ -1,2 +1,38 @@
 console.log('Password Manager content script loaded')
 console.log(window.location.href)
+
+const detectedPasswordFields = new WeakSet<HTMLInputElement>()
+
+function logPasswordField(passwordField: HTMLInputElement) {
+  if (detectedPasswordFields.has(passwordField)) {
+    return
+  }
+
+  detectedPasswordFields.add(passwordField)
+  console.log('Password field detected')
+  console.log(passwordField)
+}
+
+function detectPasswordFields(root: ParentNode) {
+  if (root instanceof HTMLInputElement && root.type === 'password') {
+    logPasswordField(root)
+  }
+
+  root
+    .querySelectorAll<HTMLInputElement>('input[type="password"]')
+    .forEach(logPasswordField)
+}
+
+detectPasswordFields(document)
+
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    for (const node of mutation.addedNodes) {
+      if (node instanceof Element) {
+        detectPasswordFields(node)
+      }
+    }
+  }
+})
+
+observer.observe(document.documentElement, { childList: true, subtree: true })
